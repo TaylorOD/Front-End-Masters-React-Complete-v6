e782df566c30c683a31967e9e685bfd12e27fd55
taylorOD <taylorsunwantedemails@gmail.com>
2022-04-11 13:21:11 -0700

    add all tooling

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,22 +1,27 @@
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ])
-}
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {name: "Natalie", animal: "Dog", breed: "Shepard"}),
-      React.createElement(Pet, {name: "Bubbles", animal: "Fish", breed: "Beta"}),
-      React.createElement(Pet, {name: "Bambi", animal: "Deer", breed: "Whitetail"}),
-    ]
-  )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Natalie",
+      animal: "Dog",
+      breed: "Shepard",
+    }),
+    React.createElement(Pet, {
+      name: "Bubbles",
+      animal: "Fish",
+      breed: "Beta",
+    }),
+    React.createElement(Pet, {
+      name: "Bambi",
+      animal: "Deer",
+      breed: "Whitetail",
+    }),
+  ]);
+};
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"))
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
