454fb6d436a846ed75762be091ccf2227a3affee
taylorOD <taylorsunwantedemails@gmail.com>
2022-04-13 15:38:35 -0700

    add class components

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,37 +1,36 @@
+import { StrictMode } from "react";
 import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import ReactDOM from "react-dom";
-
-// import Pet from "./Pet";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <header>
+          <Link to="/">
+            <h1>Adopt Me!</h1>
+          </Link>
+        </header>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt Me!"),
-//     React.createElement(Pet, {
-//       name: "Natalie",
-//       animal: "Dog",
-//       breed: "Shepard",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Bubbles",
-//       animal: "Fish",
-//       breed: "Beta",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Bambi",
-//       animal: "Deer",
-//       breed: "Whitetail",
-//     }),
-//   ]);
-// };
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
